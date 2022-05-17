beb275198a4ea92b72ca1db64e65417c4becb789
originilala <jonathan.dewsaran@gmail.com>
2022-05-17 17:00:39 +0200

    Make files for all assignments

---
+++ opdracht1.js
@@ -0,0 +1,21 @@
+import inventory from "./inventory.js";
+
+/*
+Opdracht 1a: Gebruik een array-methode om een array te maken met alle tv-type namen. Log de uitkomst in de console.
+
+    pseudo code
+
+Input:
+-Array
+
+Output:
+-Array met entries met tv type namen
+-Log in de console
+
+Stappen:
+-Loop door array
+-Selecteer bij iedere entry de tv type naam
+-Sla de tv type naam op
+-Maak van alle tv type namen een array
+*/
+

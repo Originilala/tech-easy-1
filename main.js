db4fd5a1b4a315fdee0a65bcc28c099050075b57
originilala <jonathan.dewsaran@gmail.com>
2022-05-17 21:38:58 +0200

    Finish assignment 1

---
+++ main.js
@@ -0,0 +1,11 @@
+import {tvTypes,
+        tvSold,
+        tvAmbi,
+        tvPriceSort} from "./opdracht1.js";
+
+
+console.log("opdracht 1a", tvTypes);
+console.log("opdracht 1b", tvSold);
+console.log("opdracht 1c", tvAmbi);
+console.log("opdracht 1d", tvPriceSort);
+
